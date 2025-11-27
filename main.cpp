64be9e86c27747e25ee0b4c837b74d173c76211c
arishamohsinn99 <arishamohsinn@gmail.com>
2025-11-27 19:38:31 +0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,10 +2,7 @@
 using namespace std;
 int main()
 {
-     //code for mall management system
-     cout<<"============================"<<endl;
-     cout<<"  MALL MANAGEMENT SYSTEM"<<endl;
-     cout<<"============================"<<endl;
-     cout<<"hello"<<endl;
+    
+     cout<<"hello";
 
 }
