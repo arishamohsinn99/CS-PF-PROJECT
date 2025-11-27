cc39ba94d7a7589b1f73d0fb65a82fe8b5896af5
arishamohsinn99 <arishamohsinn@gmail.com>
2025-11-27 20:41:51 +0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,26 +2,28 @@
 using namespace std;
 int main()
 {
-    
-     cout<<"______________________________________"<<endl;
-     cout<<"|                                     |"<<endl;
-     cout<<"|                                     |"<<endl;
-     cout<<"|     ★ MALL MANAGEMENT SYSTEM ★     |"<<endl;
-     cout<<"|                                     |"<<endl;
-     cout<<"|  =================================  |"<<endl;
-     cout<<"|  |                               |  |"<<endl;
-     cout<<"|  |           * LOGIN *           |  |"<<endl;
-     cout<<"|   ===============================   |"<<endl;
-     cout<<"|  |                               |  |"<<endl;
-     cout<<"|  |                               |  |"<<endl;
-     cout<<"|  |                               |  |"<<endl;
-     cout<<"|  |                               |  |"<<endl;
-     cout<<"|  |                               |  |"<<endl;
-     cout<<"|  |                               |  |"<<endl;
-     cout<<"|  |                               |  |"<<endl;
-     cout<<"|  |                               |  |"<<endl;
-     cout<<"|  |                               |  |"<<endl;
-     cout<<"|  =================================  |"<<endl;
+    string username, password;
+     cout<<"__________________________________________"<<endl;
+     cout<<"|                                         |"<<endl;
+     cout<<"|                                         |"<<endl;
+     cout<<"|     ★ MALL MANAGEMENT SYSTEM ★         |"<<endl;
+     cout<<"|                                         |"<<endl;
+     cout<<"|  ====================================   |"<<endl;
+     cout<<"|  |                                   |  |"<<endl;
+     cout<<"|  |           * LOGIN *               |  |"<<endl;
+     cout<<"|   ===================================   |"<<endl;
+     cout<<"|  |                                   |  |"<<endl;
+     cout<<"|  |                                   |  |"<<endl;
+     cout<<"|  |    USERNAME: __________________   |  |"<<endl;
+     getline(cin,username);
+     cout<<"|  |    PASSWORD: __________________   |  |"<<endl;
+     getline(cin,password);
+     cout<<"|  |                                   |  |"<<endl;
+     cout<<"|  |                                   |  |"<<endl;
+     cout<<"|  |                                   |  |"<<endl;
+     cout<<"|  |                                   |  |"<<endl;
+     cout<<"|  |                                   |  |"<<endl;
+     cout<<"|  =====================================  |"<<endl;
      
 
 
