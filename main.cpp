2a2999940e60243ab56a3074077658ae64dc6bee
arishamohsinn99 <arishamohsinn@gmail.com>
2025-11-27 20:33:39 +0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,6 +3,27 @@ using namespace std;
 int main()
 {
     
-     cout<<"hello";
+     cout<<"______________________________________"<<endl;
+     cout<<"|                                     |"<<endl;
+     cout<<"|                                     |"<<endl;
+     cout<<"|     ★ MALL MANAGEMENT SYSTEM ★     |"<<endl;
+     cout<<"|                                     |"<<endl;
+     cout<<"|  =================================  |"<<endl;
+     cout<<"|  |                               |  |"<<endl;
+     cout<<"|  |           * LOGIN *           |  |"<<endl;
+     cout<<"|   ===============================   |"<<endl;
+     cout<<"|  |                               |  |"<<endl;
+     cout<<"|  |                               |  |"<<endl;
+     cout<<"|  |                               |  |"<<endl;
+     cout<<"|  |                               |  |"<<endl;
+     cout<<"|  |                               |  |"<<endl;
+     cout<<"|  |                               |  |"<<endl;
+     cout<<"|  |                               |  |"<<endl;
+     cout<<"|  |                               |  |"<<endl;
+     cout<<"|  |                               |  |"<<endl;
+     cout<<"|  =================================  |"<<endl;
+     
+
+
 
 }
